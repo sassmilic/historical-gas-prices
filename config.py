47a6d53cff3a49cc728148f27bad2ee752796683
Sasa Milic <zyklonbb@protonmail.com>
2020-06-26 23:36:50 -0400

    made multithreaded

--- config.py
+++ config.py
@@ -1,10 +1,11 @@
 import json
 import multiprocessing
 
-INFURA_URL = "" #delet
-INFURA_PROVIDER = "" #delet
+INFURA_URL = "https://mainnet.infura.io/v3/87dde21eb7704f8c82f3b51aa58653d0" #delet
+INFURA_PROVIDER = "wss://mainnet.infura.io/ws/v3/87dde21eb7704f8c82f3b51aa58653d0" #delet
 
 # configs for scraping transaction prices
 PERCENTILE_GRANULARITY = 20
+SAMPLE_PERCENT = 1
 NTHREADS = multiprocessing.cpu_count()
 TIMEZONE = 'utc'
--- scrape.py
+++ scrape.py
@@ -1,33 +1,33 @@
 from datetime import datetime, timedelta, timezone
 import numpy as np
+from numpy.random import choice
 import pandas as pd
+import queue
 import threading
 import time
 from multiprocessing import Pool
+
+from hanging_threads import start_monitoring
+monitoring_thread = start_monitoring()
 
 from util import LockedIterator, connect, get_first_eth_block_at
 
 from config import (
     PERCENTILE_GRANULARITY,
-    NTHREADS
+    NTHREADS,
+    SAMPLE_PERCENT,
 )
+
+WEB3 = connect()
 
 to_unixtime = lambda dt: (dt - datetime(1970, 1, 1)).total_seconds()
 dt_to_str = lambda dt: dt.strftime('%Y-%m-%d')
+do_select = lambda: choice([True, False], 1, p=[SAMPLE_PERCENT/100, 1 - SAMPLE_PERCENT/100])[0]
 
 # TODO: multithreading
 # TODO: configurable aggregation granularity -- currently set at 1 day
 # TODO: should I continue weighing gas price by total gas used in transaction?
 # TODO: timezones
-
-class Transaction:
-    '''
-    Keeping only info relevant to gas prices.
-    '''
-    def __init__(self, block_number, gas, gas_price):
-        self.block_number = block_number
-        self.gas = gas
-        self.gas_price = gas_price
 
 def scrape_prices(dt_from, dt_to=None, outfile='gas_prices.csv'):
    
@@ -42,50 +42,99 @@ def scrape_prices(dt_from, dt_to=None, outfile='gas_prices.csv'):
     else:
         block2 = web3.eth.getBlock('latest')
 
+    block_nums = range(block1['number'], block2['number'] + 1) 
+
+    ##
+    ## producer - consoomer pattern
+    ##
+
+    txn_queue = queue.Queue()
+    price_queue = queue.Queue()
+    
+    # Create consumers
+    # - consoomers read from txn hash queue and query transactions for prices
+    for i in range(NTHREADS - 1):
+        t = threading.Thread(target=consoomer, args=(i, txn_queue, price_queue))
+        # thread killed once main program exits
+        t.daemon = True
+        t.start() 
+   
+    # create producer
+    # producer queries for block numbers and collects txn hashes
+    result = {}
+    prod = threading.Thread(target=producer, args=(block_nums, txn_queue, price_queue, result))
+    prod.start() 
+    prod.join()
+
+    print("RESULT")
+    print(result)
+    df = pd.DataFrame(result)
+    df.to_csv(outfile, header=True)
+
+
+# Function called by the producer thread
+def producer(block_nums, txn_queue, price_queue, agg_result):
+    web3 = connect()
+    
     percents =  np.linspace(0, 100, PERCENTILE_GRANULARITY + 1, endpoint=True)
     percents = percents[1:] # remove 0
-    
+  
     columns = ['day', 'total_txns', *percents]
-    result = {col:[] for col in columns}
-
-    block_num_gen = range(block1['number'], block2['number'] + 1) 
+    for col in columns:
+        agg_result[col] = []
 
     current_date = None # granularity at day-level
-    txns = []
 
-    def _aggregate():
-        result['day'].append(dt_to_str(current_date))
-        result['total_txns'].append(len(txns))
-        pcts = np.percentile([t.gas_price / t.gas for t in txns], percents)
-        for k,v in zip(percents, pcts):
-            result[k].append(v)
-
-    for i,block_num in enumerate(block_num_gen):
-
+    for block_num in block_nums:
+        
         block = web3.eth.getBlock(block_num)
-
         date = datetime.utcfromtimestamp(block['timestamp'])
+       
+        print(date)
+
         if not current_date:
             current_date = date
-
-
+        
         # aggregate per day
         if date - current_date >= timedelta(days=1):
             current_date = date
-            _aggregate()
-            txns = []
+            __aggregate(price_queue, txn_queue, agg_result, percents, current_date)
 
         for txnhash in block['transactions']:
-            txns.append(__parse_transaction(txnhash))
+            # only add SAMPLE_PERCENT of transactions to txn queue
+            if do_select():
+                txn_queue.put(txnhash)
 
-    df = pd.DataFrame(result)
-    df.to_csv(outfile, columns=columns)
+def __aggregate(price_queue, txn_queue, agg_result, percents, current_date):
+    print('aggregating', dt_to_str(current_date))
+    prices = []
+    count = 0
 
-def __parse_transaction(txnhash):
+    # this will block until consumer threads are done
+    while not (price_queue.empty() and txn_queue.empty()):
+        if not price_queue.empty():
+            prices.append(price_queue.get())
+            count += 1
+        else:
+            # wait for price queue to populate
+            time.sleep(1)
+    
+    agg_result['day'].append(dt_to_str(current_date))
+    agg_result['total_txns'].append(count)
+    pcts = np.percentile(prices, percents)
+    for k,v in zip(percents, pcts):
+        # dicts and lists are threadsafe, I think
+        agg_result[k].append(v)
+
+def consoomer(i, txn_queue, price_queue):
     web3 = connect()
-    txn = web3.eth.getTransaction(txnhash)
-    return Transaction(txn['blockNumber'], txn['gas'], txn['gasPrice'])
-
+    while True:
+        try:
+            txnhash = txn_queue.get()
+            txn = web3.eth.getTransaction(txnhash)
+            price_queue.put(txn['gasPrice'] / txn['gas'])
+        except queue.Empty:
+            pass 
 
 if __name__ == '__main__':
     datestr = '2020-06-20'
